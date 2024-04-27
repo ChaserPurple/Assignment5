352f3fe2cacdf8c08bb8e028348c07fe204459d9
unknown <mcar3550@students.kleinisd.net>
2024-04-27 16:07:15 -0500

    fuck all of this

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -6,9 +6,13 @@ from app.CustomerData import CustomerData
 from fastapi import Request
 from typing import Dict, Any
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import select
 
 DATABASE_URL = "mysql+mysqlconnector://root:supersecretpassw0rd@localhost/sakila"
 
+engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(engine)
 
 app = FastAPI()
 
@@ -22,35 +26,39 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def executeSelect(query: str):
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def executeSelect(table: str, attributes: list[str], end: str = ""):
+    query: str = "SELECT "
 
-    session = SessionLocal()
-    try:
-        result = session.execute(text(query))
-        return [dict(r._mapping) for r in result]
-    finally:
-        session.close()
+    for i in range(0, len(attributes)):
+        query = f"{query}{attributes[i]}"
+        if i < len(attributes) - 1:
+            query = f"{query}, "
+    
+    query = f"{query} FROM {table} {end}"
+
+    with engine.connect() as conn:
+        rows = conn.execute(text(query))
+        conn.commit()
+        return [dict(zip(attributes, r)) for r in rows]
+
+
+# def executeSelect(query: str):
+#     with engine.connect() as conn:
+#         rows = conn.execute(text(query))
+#         conn.commit()
+#         return [for r in rows]
 
 def executeInsert(query: str):
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-    session = SessionLocal()
-    try:
-        result = session.execute(text(query))
-        return result
-    finally:
-        session.close()
+    with engine.connect() as conn:
+        rows = conn.execute(text(query))
+        conn.commit()
+        row = rows.lastrowid
+        return row
 
 @app.get("/")
 async def root():
     return {"message": "Howdy"}
 
-@app.get("/getCountry")
-def getCountry():
-    return executeSelect("SELECT * from country")
 
 @app.get("/getCanadianCustomers")
 def getCanadianCustomers():
@@ -68,9 +76,22 @@ def getCanadianCustomers():
 
 @app.get("/getStoreFilms/")
 def getStoreFilm(store_id: int = 1):
-    query: str = f"select f.film_id, f.title, f.release_year, f.length, f.rating, f.rental_rate from (film as f inner join inventory as i on f.film_id=i.film_id) where i.store_id={store_id} and i.inventory_id not in (select distinct inventory_id from rental as r where r.return_date is null) group by f.film_id;"
-    return executeSelect(query)
+    s = statement = text("""SELECT * FROM film""")
+    with engine.connect() as conn:
+        rows = conn.execute(s)
+        print(rows.all())
     
+@app.get("/getCustomer")
+async def getCustomer(payload: Request):
+    data = await payload.json()
+    fname = data['fname']
+    lname = data['lname']
+    phone = data['phone']
+    result = executeSelect('customer join address on customer.address_id=address.address_id', ['customer_id'], f"where first_name='{fname}' and last_name='{lname}' and phone='{phone}'")
+    row_id = -1
+    if len(result) == 1:
+        row_id = result[0]['customer_id']
+    return row_id
 
 @app.post("/createCustomer")
 def createCustomer():
@@ -85,22 +106,45 @@ def createCustomer():
 
 @app.get("/getFilms")
 def getFilms():
+    result = executeSelect('film', ['film_id', 'title', 'release_year', 'rating', 'rental_rate'])
     query: str = """
         SELECT film.film_id, film.title, film.release_year, film.length, film.rating, film.rental_rate
         from film
     """
-    return executeSelect(query)
+    # result = executeSelect(query)
+    print(result)
+    return result
 
 def getCityID(city: str):
     query: str = f"select city_id from city where city='{city}'"
     return executeSelect(query)
 
-def createAddress(address: str, city: str, state: str, zip_code: str):
-    city_id = getCityID(city)
-    if len(city_id) == 1:
-        query: str = f"insert into address (address, district, city_id, postal_code, phone, )"
+def createAddress(address: str, city: str, state: str, zip_code: str, phone_number: str):
+    cities = getCityID(city)
+    row = -1
+    # if len(cities) == 1:
+    #     city_id = cities[0]['city_id']
+    #     query = text(with engine.connect() as conn:
+    #         rows = conn.execute(query)
+    #         conn.commit()
+    #         row = rows.lastrowid
+    # return row
 
     
+@app.post("/addAddress")
+async def createAddress(payload: Request):
+    jsn = await payload.json()
+    address = jsn['address']
+    city = jsn['city']
+    district = jsn['district']
+    zip_code = jsn['zip_code']
+    phone_number = jsn['phone_number']
+    cities = getCityID(city)
+    row = -1
+    if len(cities) == 1:
+        city_id = cities[0][0]
+        row = executeInsert(f"insert into address (address, district, city_id, postal_code, phone, location) values('{address}', '{district}', {city_id}, '{zip_code}', '{phone_number}', POINT('39.8333333','-98.585522'))")
+    return row
 
 @app.post("/addCustomer")
 async def addCustomer(payload: Request):
